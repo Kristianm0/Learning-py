259103f9491f33183c3e9a0be9f734786ea6b3e1
Kristianm <krisjaviermartinez@gmail.com>
2022-03-11 10:09:27 -0500

    Clase 20 funciones de cadena de texto

--- c20.py
+++ c20.py
@@ -1,6 +1,11 @@
 ##Funciones de cadena de texto
+
 krism = "Hola kristian, Espero que estes bien"
 
-modi = krism.capitalize()
-print (modi)
-p
+modi = krism.split()
+print (modi [1])
+
+lista = "Luis,Maria,Pedro,Luisa"
+lista_nombres = lista.split(",")
+
+print(lista_nombres)
