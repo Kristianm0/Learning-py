cda2643b9412a22bb35c5055f937f644d3e2b62f
Kristianm <krisjaviermartinez@gmail.com>
2022-03-11 09:27:43 -0500

    Cadenas de texto + opciones aleatorias

---
+++ c19.py
@@ -0,0 +1,32 @@
+## Cadenas de textos o listas
+
+
+numve = "0414 630 5117" 
+numco = "302 500 129"
+
+
+import random
+hi = ['Espero que estes bien, ' , 'Me alegra hablar contigo, ' , 'No te preocupes que aqui saldras si dudas, ']
+user = ['Kevin, ' , 'Katalina, ' , 'Rodo, ' , 'Amokita, ']
+
+for k in range (1) :
+    (random.choice(hi)) 
+for r in range (1) :
+    (random.choice(user))
+
+hi1 = (random.choice(hi)) 
+user1 = (random.choice(user))
+
+print ((user1 )  +  (hi1)) 
+
+#letra = hi[1]
+#print(letra)
+
+#letra = hi[-1]
+#print(letra)
+
+letra = numve[9:13]
+codi = " Es es tu codigo random."
+print (letra + codi)
+
+
