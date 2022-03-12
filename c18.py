ccb224db387a49151d7fb621b1e5d41a6873c3a5
Kristianm <krisjaviermartinez@gmail.com>
2022-03-12 08:18:40 -0500

    ...

--- c18.py
+++ c18.py
@@ -6,10 +6,11 @@ krism = "Kristianm Colina"
 
 #print ("K" not in krism)
 
-a = 10 
+a = 10
 b = 10
 
-print (a is not float)
+print (float(a)
 
 ## Se llamo a el var para confirmar si habia un numero.
 ## Pero tambien se aprendio in y not in para verificar valores.
+# Float te devuelve un numero con decimal.
---
+++ c21.py
@@ -0,0 +1,10 @@
+### UNIENDO VARIABLES Y CADENAS
+
+krism = 102922
+colina = "Colina"
+
+krism = int(krism)
+
+ o = "Kristian es una persona muy amable" + krism + "es su nivel de amabilidad"
+
+ prim
