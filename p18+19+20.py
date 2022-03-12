bb4c898db271b001ca15e69d0d1abd27969239d9
Kristianm <krisjaviermartinez@gmail.com>
2022-03-12 07:24:42 -0500

    No pude unir todo lo que aprendi en un solo proyecto

--- p18+19+20.py
+++ p18+19+20.py
@@ -1,2 +1,42 @@
 ## Mini proyecto con las 3 clases de python (18, 19, 20) para reforzar los conocimientos
 
+#Se va a hacer una lista que se eliga los valores de manera random, esto con la etiqueta "import random" y que se define la 
+# Y luego se ejecuta y se invoca la etiqueta con "for k in range " .
+# cantidad de valores que va a atraer "(1)" , y abajo se el var con los valores a buscar ["XXXX"].
+# Y se llama a la funcion "randow.choice" y se le dice que busque en (h1).
+
+import random
+hi = ['Espero que estes bien, ' , 'Me alegra hablar contigo, ' , 'No te preocupes que aqui saldras si dudas, ' , "Super que hablemos" , "Listo para ayudar"]
+nam_s = [" Amoka", ' Katalina', ' Rodo' , ' Kevin', ' Kth']
+
+
+for k in range (1) :
+    (random.choice(hi))
+
+for u in range (1) :
+    (random.choice(nam_s))
+
+
+hi1 = (random.choice(hi)) 
+na = (random.choice(nam_s))
+
+
+print((hi1) + (na))
+
+######
+
+#Convertir  una lista en string
+#A = ["a", "b", "c"]
+#StrA = "".join(hi)
+#print(StrA)
+## StrA is "abc"
+
+
+
+
+#####
+
+
+
+
+## Te quedastes buscando una lista para poner algo. Y seguir con el mini proyecto de unir lo que sabes.
