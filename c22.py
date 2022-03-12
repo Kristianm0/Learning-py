cab0892d60875944fbd68f6699682e0fd1f52b65
Kristianm <krisjaviermartinez@gmail.com>
2022-03-12 09:39:43 -0500

    Pedir datos al usuario

---
+++ c22.py
@@ -0,0 +1,13 @@
+###introduccit texto
+# La function input.
+
+# Forma 1
+print("Ingresa tu name")
+name = input()
+print(f"El brother se llama: {name}")
+
+# Forma 2
+namem = input("Dame tu name")
+print(f"Entonces, tu nombre es: {namem}")
+
+#Hay 2 formas de hacer pedirle datos al usuario y ahi estan, la 2 es mas facil y sencilla. 
