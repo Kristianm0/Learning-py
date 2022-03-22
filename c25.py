6a521c0cb4d58d8dafb67324b7d6d6f9db72ded4
Kristianm0 <krisjaviermartinez@gmail.com>
2022-03-22 07:47:29 -0500

    Historia dinamica

---
+++ c25.py
@@ -0,0 +1,24 @@
+### Juego de historias locas
+
+
+# Declaramos las var
+# Usamos input para que el usuario ingrese lo que pedimos.
+
+print("Crea tu historia loca")
+print("Ingresa las palabras que te pedimos: \n")
+
+pal0 = input("Who is the Protagonist:\n")
+pal1 = input("What was the protagonist one day\n")
+pal2 = input("Why do that:\n")
+pal33 = input("The problem to do that:\n")
+pal3 = input("The solution for the problem:\n")
+pal4 = input("What do you learn about this problem\n")
+
+
+# Imprimimos de la historia
+print(f"This is your story protagonistized {pal0.capitalize()}")
+print(f"One day he/she or it (the pronuons isnt important right now) {pal1.lower()}\n")
+print(f"why was it doing that? A: {pal2.lower()}")
+print(f"And that activity have a problem, {pal33.lower()}")
+print(f"But dont worrid, there is a solution: {pal3.lower()}")
+print(f"So, this story told us a that there is no a problems that do not have a solution{pal4.lower()}")
