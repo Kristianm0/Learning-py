3eff5c64589e1e2faf57ef360ab6c8e3c626eb93
Kristianm0 <krisjaviermartinez@gmail.com>
2022-04-20 07:10:19 -0500

    Funciones lambda

---
+++ c30.py
@@ -0,0 +1,8 @@
+### Funciones lambda 
+## Funciones anonimas que se ejecutan con una sola expresion.
+
+
+
+result = lambda numero: numero + 20
+suma = lambda numero1: numero3 + 200
+print(result(200))
