9466127c0019f46ac7dd2520b283764ac15839d8
Kristianm0 <krisjaviermartinez@gmail.com>
2022-05-03 06:59:41 -0500

    conditional

---
+++ c32.py
@@ -0,0 +1,17 @@
+## conditionals
+
+hola = print("""¿Quieres escribir tu mensaje? elige un numero
+1. Si 
+2. No
+""")
+
+mensa = input()
+
+if mensa == "1":
+    input("Escribe ")
+
+elif mensa == "2":
+    print("No escribas nada")
+
+else: 
+    print("😄 No hay opciones disponibles")
