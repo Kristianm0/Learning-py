2e36eddff71484cac7494b4ddba082c00fd2da27
Kristianm0 <krisjaviermartinez@gmail.com>
2022-05-10 07:45:42 -0500

    git

---
+++ c_37_dictionary.py
@@ -0,0 +1,22 @@
+### Dictionary ###
+
+days = {
+    "MO": "Monday",
+    "TU": "Tuesday",
+    "WE": "Wednesday",
+    "TH": "Thuesday",
+    "FR": "Friday",
+    "SA": "Saturday",
+    "SU": "Sunday"
+}
+
+## Find data
+# print(days.get())
+
+## Remove data
+# print(days.popitem())
+
+## Add new data
+#print(days.update(M = "Lunes"))
+#print(days)
+
