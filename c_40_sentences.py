f38c8c90628edb3ae12e9374bbd23f113aeaa67d
Kristianm0 <krisjaviermartinez@gmail.com>
2022-05-12 07:30:07 -0500

    main

---
+++ c_40_sentences.py
@@ -0,0 +1,15 @@
+### Sentences, break and continues
+
+days = {"Mo": "Monday", "Tu": "Tuesday", "We": "Wednesday"}
+
+
+#for pw, values in days:
+#print(days)
+
+#for index in range(10):
+#   for index in range(3):
+#      print(index, index)
+
+for index in range(10):
+    if index == 4:
+        break
