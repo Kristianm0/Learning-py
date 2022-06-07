8b0e8592cd0a6ef13e3e86397d6984b7c30ad12e
Kristianm <101115738+Kristianm0@users.noreply.github.com>
2022-06-07 07:24:13 -0500

    python basic

---
+++ c50_ex.py
@@ -0,0 +1,18 @@
+
+
+from random import random
+
+
+hoa = "luis", "pedro"
+names = 100
+
+if names == 100:
+    print("hoa")
+    h = input(">>> ")
+    hh = 1
+    print(h + "😱")
+
+if hh == 1:
+    print("#")
+
+u = print(random.randint(12, 0))
