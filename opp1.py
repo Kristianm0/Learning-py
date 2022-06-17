23cdba959500cdae3ef2a39d05368f4d669f9573
Kristianm <101115738+Kristianm0@users.noreply.github.com>
2022-06-17 08:35:50 -0500

    Add files via upload

---
+++ opp1.py
@@ -0,0 +1,20 @@
+class Person:
+    profession = "Teacher"
+    name = "Juan"
+    last_name = "Perez"
+
+Person.profession
+#print(Person.profession)
+
+class name:
+    pass 
+vikram = name()
+lili = name()
+
+vikram.age = 20
+vikram.gender = "male"
+vikram.country = 'Irak'
+
+print(vikram.age)
+
+#objeto.atributo = valor
