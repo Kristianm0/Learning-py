0a70fe062dad5ce8511ea3a845e8f37c460f87c7
Krism <krisjaviermartinez@gmail.com>
2022-08-01 07:56:11 -0500

    programacion opp 2 clase

---
+++ 2opp.py
@@ -0,0 +1,39 @@
+#Class and object#
+
+
+#class People:
+#   doctor = "Amoka"
+
+#print(People.doctor)
+
+#class Players_A:
+    #p1 = "Amoka"
+    #p2 = "Rodolfo"
+
+#class Players_B:
+    #p2 = "Katalina"
+    #p3 = "Rodo"
+
+#class Team:
+    #t2 = "Barcelona"
+    #t3 = "Real Madrid"
+
+#Dont forget the structure:
+#object.attribute = Value
+
+class Name:
+    pass
+
+victor = Name()
+maria = Name()
+
+victor.age = 30
+victor.sexo = "Masculino"
+victor.country = "Bolivia"
+
+maria.age = 25
+maria.sexo = "Femenino"
+maria.country = "Colombia"
+
+print(victor.age)
+print(maria.age)
